42deb8bd4b18c71c55f459c06f28152bb611ab48
Taptaplit <vismayp301@gmail.com>
2020-11-23 21:44:24 -0500

    dark/light mode update

--- mode.js
+++ mode.js
@@ -7,11 +7,21 @@ const darkMode = localStorage.getItem('darkMode');
 
 const enableDarkMode = () => {
     document.getElementById('mode').setAttribute('href', 'web-dark.css');
+    document.getElementById('img1').setAttribute('src', 'https://futurefive.co.nz/uploads/story/2020/02/18/gamer_room.jpg');
+    document.getElementById('img2').setAttribute('src', 'Boring-Games-logo.png');
+    document.getElementById('img3').setAttribute('src', 'https://futurefive.co.nz/uploads/story/2020/02/18/gamer_room.jpg');
+    document.getElementById('img4').setAttribute('src', 'https://wallpaperaccess.com/full/185915.jpg');
+    document.getElementById('img5').setAttribute('src', 'https://futurefive.co.nz/uploads/story/2020/02/18/gamer_room.jpg');
     localStorage.setItem("darkMode", "enabled");
 };
 
 const disabledDarkMode = () => {
     document.getElementById('mode').setAttribute('href', 'web-light.css');
+    document.getElementById('img1').setAttribute('src', 'https://i.pinimg.com/originals/85/6f/31/856f31d9f475501c7552c97dbe727319.jpg');
+    document.getElementById('img2').setAttribute('src', 'https://static.vecteezy.com/system/resources/thumbnails/001/072/376/original/network-connections-on-white-background.jpg');
+    document.getElementById('img3').setAttribute('src', 'https://i.pinimg.com/originals/46/1d/25/461d254f8563f8945ce3a385f289df17.jpg');
+    document.getElementById('img4').setAttribute('src', 'https://www.lefthudson.com/wp-content/uploads/2019/11/white-background-wallpaper-awesome-white-wallpapers-in-hd-barbaras-hd-wallpapers-ideas-of-white-background-wallpaper.jpg');
+    document.getElementById('img5').setAttribute('src', 'https://wallpaperaccess.com/full/410244.jpg');
     localStorage.setItem("darkMode", null);
 };
 
